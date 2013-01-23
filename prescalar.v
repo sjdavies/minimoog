a4bae4f5253beefaeb5ef680e74fe44220c565f8
SteveD <SteveD@nowhere.com>
2013-01-23 20:40:35 +1000

    File cleanup (spaces, tabs etc.)

--- prescalar.v
+++ prescalar.v
@@ -1,6 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company:        Davies Consulting Pty. Ltd.
 // Engineer:       Stephen Davies
 // 
 // Create Date:    17:30:54 18 December 2012 
@@ -9,63 +8,74 @@
 // Project Name:   MiniMoog Synth simulator
 // Target Devices: Spartan 6
 // Tool versions:  ISE 14.4
-// Description:    Use the Spartn 6 DCM resources to generate the various clocks required.
+// Description:    Use a Spartan 6 DCM resource to generate buffered 32MHz and 16MHz clocks.
 //
 // Dependencies: 
 //
-// Revision:       0.01
+// Revision:       0.02
 // Revision 0.01 - File Created
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
 
 module prescalar(
-    input clkin,
-    output clk0,
-	 output clk16,
-	 output clk16_180
+    input clkin,        // raw clock signal from external clock source
+    
+    output clk0,        // Buffered 32MHz clock
+    output clk16,       // Buffered 16 MHz clock
+    output clk16_180    // Buffered 16 MHz clock (180 degrees out of phase with clk16)
     );
 
-	wire bufferedIn;
-	wire unbuf0;
-	wire unbuf16;
-	wire unbuf16_180;
+    wire bufferedIn;
+    wire unbuf0;
+    wire unbuf16;
+    wire unbuf16_180;
 	
-   IBUFG inBuf       (.O(bufferedIn), .I(clkin));
-	BUFG clkOut0      (.O(clk0), .I(unbuf0));	
-	BUFG clkOut16     (.O(clk16), .I(unbuf16));
-	BUFG clkOut16_180 (.O(clk16_180), .I(unbuf16_180));
+    //
+    // Use Xilinx primitives to buffer in and out clock signals to/from DCM.
+    //
+    IBUFG   inBuf         (.O(bufferedIn), .I(clkin));
+    BUFG    clkOut0       (.O(clk0), .I(unbuf0));	
+    BUFG    clkOut16      (.O(clk16), .I(unbuf16));
+    BUFG    clkOut16_180  (.O(clk16_180), .I(unbuf16_180));
 	
-	//
-	// Buffer the 32 MHz system clock and generate a 16 MHz clock for use by the SPI ports.
-	//
-   DCM_SP #(
-      .CLKFX_DIVIDE(4),                     // Divide value on CLKFX outputs - D - (1-32)
-      .CLKFX_MULTIPLY(2),                   // Multiply value on CLKFX outputs - M - (2-32)
-      .CLKIN_DIVIDE_BY_2("FALSE"),          // CLKIN divide by two (TRUE/FALSE)
-      .CLKIN_PERIOD(31.25),                 // Input clock period specified in nS
-      .CLKOUT_PHASE_SHIFT("NONE"),          // Output phase shift (NONE, FIXED, VARIABLE)
-      .CLK_FEEDBACK("1X"),                  // Feedback source (NONE, 1X, 2X)
-      .DESKEW_ADJUST("SYSTEM_SYNCHRONOUS"), // SYSTEM_SYNCHRNOUS or SOURCE_SYNCHRONOUS
-      .DFS_FREQUENCY_MODE("LOW"),           // Unsupported - Do not change value
-      .DLL_FREQUENCY_MODE("LOW"),           // Unsupported - Do not change value
-      .DSS_MODE("NONE"),                    // Unsupported - Do not change value
-      .DUTY_CYCLE_CORRECTION("TRUE"),       // Unsupported - Do not change value
-      .FACTORY_JF(16'hc080),                // Unsupported - Do not change value
-      .PHASE_SHIFT(0),                      // Amount of fixed phase shift (-255 to 255)
-      .STARTUP_WAIT("FALSE")                // Delay config DONE until DCM_SP LOCKED (TRUE/FALSE)
-   )
-   main_dcm (
-      .CLKIN(bufferedIn),  	// 1-bit input: Clock input
-      .CLK0(unbuf0),	         // 1-bit output: 0 degree clock output
-      .CLKFB(clk0),      	   // 1-bit input: Clock feedback input
-      .CLKFX(unbuf16),        // 1-bit output: Digital Frequency Synthesizer output (DFS)
-      .CLKFX180(unbuf16_180), // 1-bit output: 180 degree CLKFX output
-      .DSSEN(1'b0),         	// 1-bit input: Unsupported, specify to GND.
-      .PSCLK(1'b0),         	// 1-bit input: Phase shift clock input
-      .PSEN(1'b0),          	// 1-bit input: Phase shift enable
-      .PSINCDEC(1'b0),      	// 1-bit input: Phase shift increment/decrement input
-      .RST(1'b0)            	// 1-bit input: Active high reset input
-   );
+    //
+    // Buffer the 32 MHz system clock and generate a 16 MHz clock for use by the SPI ports.
+    //
+    DCM_SP #(
+        // clkin * 2 / 4 = 16 MHz
+        .CLKFX_MULTIPLY(2),                   // Multiply value on CLKFX outputs - M - (2-32)
+        .CLKFX_DIVIDE(4),                     // Divide value on CLKFX outputs - D - (1-32)
+
+        .CLKIN_DIVIDE_BY_2("FALSE"),          // CLKIN divide by two (TRUE/FALSE)
+        .CLKIN_PERIOD(31.25),                 // Input clock period specified in nS
+        .CLKOUT_PHASE_SHIFT("NONE"),          // Output phase shift (NONE, FIXED, VARIABLE)
+        .CLK_FEEDBACK("1X"),                  // Feedback source (NONE, 1X, 2X)
+
+        // standard template defaults
+        .DESKEW_ADJUST("SYSTEM_SYNCHRONOUS"), // SYSTEM_SYNCHRNOUS or SOURCE_SYNCHRONOUS
+        .DFS_FREQUENCY_MODE("LOW"),           // Unsupported - Do not change value
+        .DLL_FREQUENCY_MODE("LOW"),           // Unsupported - Do not change value
+        .DSS_MODE("NONE"),                    // Unsupported - Do not change value
+        .DUTY_CYCLE_CORRECTION("TRUE"),       // Unsupported - Do not change value
+        .FACTORY_JF(16'hc080),                // Unsupported - Do not change value
+        .PHASE_SHIFT(0),                      // Amount of fixed phase shift (-255 to 255)
+        .STARTUP_WAIT("FALSE")                // Delay config DONE until DCM_SP LOCKED (TRUE/FALSE)
+    )
+    main_dcm (
+        .CLKIN(bufferedIn),                   // Clock input
+        .CLK0(unbuf0),	                      // 0 degree clock output
+        .CLKFB(clk0),      	                  // Clock feedback input
+        
+        .CLKFX(unbuf16),                      // Digital Frequency Synthesizer output (DFS)
+        .CLKFX180(unbuf16_180),               // 1-bit output: 180 degree CLKFX output
+        
+        // standard template values
+        .DSSEN(1'b0),         	              // Unsupported, specify to GND.
+        .PSCLK(1'b0),         	              // Phase shift clock input
+        .PSEN(1'b0),          	              // Phase shift enable
+        .PSINCDEC(1'b0),      	              // Phase shift increment/decrement input
+        .RST(1'b0)            	              // Active high reset input
+    );
 
 endmodule
--- top.v
+++ top.v
@@ -19,97 +19,97 @@
 
 module top(
 
-   input				CLK,					// 32 MHz signal from crystal oscillator
+    input           CLK,                // 32 MHz signal from crystal oscillator
 	
 	// Analog output pins (connected to RC network for low pass filter)
-	output			A1L,
-	output			A1R,
-	output			A2L,
-	//output			A2R,
+    output          A1L,
+    output          A1R,
+    output          A2L,
+    //output          A2R,
 	
-	// ADC port #1
-	output			ADC1_SCK,
-	output			ADC1_nCS,
-	output			ADC1_DI,
-	input				ADC1_DO,
+    // ADC port #1
+    output          ADC1_SCK,
+    output          ADC1_nCS,
+    output          ADC1_DI,
+    input           ADC1_DO,
 
-	// ADC port #2
-	output			ADC2_SCK,
-	output			ADC2_nCS,
-	output			ADC2_DI,
-	input				ADC2_DO,
+    // ADC port #2
+    output          ADC2_SCK,
+    output          ADC2_nCS,
+    output          ADC2_DI,
+    input           ADC2_DO,
 	
-	// utility wing
-	output [7:0]  A,
+	// utility wing, use for debugging
+    output [7:0]    A,
 	
-	// Unused resources that need to be put into an inert state
-	output			SDRAM_nCS
+    // Unused resources that need to be put into an inert state
+    output          SDRAM_nCS
 	
-   );
+    );
 
-	localparam		DAC_WIDTH = 12;		// DAC size in bits
+    localparam      DAC_WIDTH = 12;     // DAC size in bits
 	
-	wire clk0;			// Buffered 32MHz clock (system clock)
-	wire clk16;			// Buffered 16Mhz clock (for ADC SPI)
-	wire clk16_180;   // Buffered 16Mhz clock (for ADC SPI)
+    wire            clk0;               // Buffered 32MHz clock (system clock)
+    wire            clk16;              // Buffered 16Mhz clock (for ADC SPI)
+    wire            clk16_180;          // Buffered 16Mhz clock (for ADC SPI)
 	
-	reg [4:0] count;
-	wire ncoTick;		// once every 32 clk cycles
-	
-	wire reset;			// Papilio Pro does not have a user RESET pin, simulate one by delaying startup
+    reg [4:0]       count;              // Divide by 32 counter, used to generate ncoTick
+    wire            ncoTick;            // once every 32 clk cycles, numerically controlled oscillators accumulate and DAC's output
 
-	wire [2:0] oscAddr;		// selects analog port to read into oscBank
-	wire [7:0] oscData;		// data value for selected analog port
-	
-	//
-	// Output busses, values are unsigned int
-	//
-	wire [DAC_WIDTH-1:0] dac1L;
-	wire [DAC_WIDTH-1:0] dac1R;
-	wire [DAC_WIDTH-1:0] dac2L;
-	//wire [DAC_WIDTH-1:0] dac2R;
-	
-	//
-	// Core system resources i.e. reset and clock generation
-	//
-	resetgen rst (.reset(reset), .clk(clk0));
-	prescalar prescale (.clk0(clk0), .clk16(clk16), .clk16_180(clk16_180), .clkin(CLK));
-	
-	//
-	// Analog to Digital Converters.
-	// Retrocade wing has 2 x ADC088S102 chips, they are polled continuously.
-	//
-	ADC adc1 (.sck(ADC1_SCK), .cs_n(ADC1_nCS), .dout(ADC1_DI), .clk(clk16), .clk180(clk16_180), .reset(reset), .din(ADC1_DO), .addr(oscAddr), .q(oscData));
-	ADC adc2 (.sck(ADC2_SCK), .cs_n(ADC2_nCS), .dout(ADC2_DI), .clk(clk16), .clk180(clk16_180), .reset(reset), .din(ADC2_DO), .addr(3'b0), .q(A[7:0]));
-	
-	//
-	// Oscillator Bank.
-	// Controlled through keyboard and adc1.
-	//
-	oscBank oscillators (.clk(clk0), .reset(reset), .note(7'd69), .bend(14'h2000), .addr(oscAddr), .data(oscData), .osc1(dac1L), .osc2(dac1R), .osc3(dac2L));
+    wire            reset;              // Papilio Pro does not have a user RESET pin, simulate one by delaying startup
 
-	//
-	// Continuous, 5 bit counter. Used to generate 1MHz 'tick' for DAC's.
-	//
-   always @(posedge clk0, posedge reset)
-		if (reset)
-			count <= 5'b0;
-		else
-			count <= count + 5'b1;
+    wire [2:0]      oscAddr;            // selects analog port to read into oscBank
+    wire [7:0]      oscData;            // data value for selected analog port
+	
+    //
+    // Output busses, values are unsigned int
+    //
+    wire [DAC_WIDTH-1:0] dac1L;
+    wire [DAC_WIDTH-1:0] dac1R;
+    wire [DAC_WIDTH-1:0] dac2L;
+    //wire [DAC_WIDTH-1:0] dac2R;
 
-	assign ncoTick = &count;
+    //
+    // Core system resources - reset and clock generation
+    //
+    resetgen    rst         (.reset(reset), .clk(clk0));
+    prescalar   prescale    (.clk0(clk0), .clk16(clk16), .clk16_180(clk16_180), .clkin(CLK));
 
-	//
-	// Digital to Analog converters
-	//
-	dsDAC #(.N(DAC_WIDTH)) dsDAC1L (.out(A1L), .in(dac1L), .clk(clk0), .nco(ncoTick), .reset(reset));
-	dsDAC #(.N(DAC_WIDTH)) dsDAC1R (.out(A1R), .in(dac1R), .clk(clk0), .nco(ncoTick), .reset(reset));
-	dsDAC #(.N(DAC_WIDTH)) dsDAC2L (.out(A2L), .in(dac2L), .clk(clk0), .nco(ncoTick), .reset(reset));
-	//dsDAC #(.N(DAC_WIDTH)) dsDAC2R (.out(A2R), .in(dac2R), .clk(clkNCO), .reset(reset));
+    //
+    // Analog to Digital Converters.
+    // Retrocade wing has 2 x ADC088S102 chips, they are polled continuously.
+    //
+    ADC         adc1        (.sck(ADC1_SCK), .cs_n(ADC1_nCS), .dout(ADC1_DI), .clk(clk16), .clk180(clk16_180), .reset(reset), .din(ADC1_DO), .addr(oscAddr), .q(oscData));
+    ADC         adc2        (.sck(ADC2_SCK), .cs_n(ADC2_nCS), .dout(ADC2_DI), .clk(clk16), .clk180(clk16_180), .reset(reset), .din(ADC2_DO), .addr(3'b0), .q(A[7:0]));
+	
+    //
+    // Oscillator Bank.
+    // Controlled through keyboard and adc1.
+    //
+    oscBank     oscillators (.clk(clk0), .reset(reset), .note(7'd69), .bend(14'h2000), .addr(oscAddr), .data(oscData), .osc1(dac1L), .osc2(dac1R), .osc3(dac2L));
 
-	//
-	// Outputs for disabled resources.
-	//
-	assign SDRAM_nCS = 1'b1;
+    //
+    // Continuous, divide by 32 counter. Used to generate 1MHz 'tick' for DAC's.
+    //
+    always @(posedge clk0, posedge reset)
+        if (reset)
+            count <= 5'b0;
+        else
+            count <= count + 5'b1;
+
+    assign ncoTick = &count;    // 1MHz 'tick' for triggering numerically 
+
+    //
+    // Digital to Analog converters
+    //
+    dsDAC   #(.N(DAC_WIDTH))    dsDAC1L (.out(A1L), .in(dac1L), .clk(clk0), .nco(ncoTick), .reset(reset));
+    dsDAC   #(.N(DAC_WIDTH))    dsDAC1R (.out(A1R), .in(dac1R), .clk(clk0), .nco(ncoTick), .reset(reset));
+    dsDAC   #(.N(DAC_WIDTH))    dsDAC2L (.out(A2L), .in(dac2L), .clk(clk0), .nco(ncoTick), .reset(reset));
+    //dsDAC #(.N(DAC_WIDTH))    dsDAC2R (.out(A2R), .in(dac2R), .clk(clk0), .nco(ncoTick), .reset(reset));
+
+    //
+    // Outputs for disabled resources.
+    //
+    assign SDRAM_nCS = 1'b1;
 	
 endmodule
